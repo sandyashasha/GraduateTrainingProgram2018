859162f8296ef850ca6708db972464d05ffc2088
Gokul Velusaamy <gokulvelusaamy@gmail.com>
2018-09-03 12:22:18 +0530

    Update even_sum.py

--- Python/Day2/even_sum.py
+++ Python/Day2/even_sum.py
@@ -1,13 +1,12 @@
 ar = []
 even_array = []
+total = 0
+
 print('Enter 10 Numbers')
 for i in range(10):
 	a = input()
 	ar.append(a)
-	
-total = 0
-for  i in ar:
-	if i%2==0:
-		total+=i
-
+	if a%2 ==0:
+		total+=a
+		
 print "Even Numbers sum is",total
