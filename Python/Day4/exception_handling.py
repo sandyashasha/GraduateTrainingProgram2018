08a36445b871b27e7e4be5c148398966c4f29e65
Gokul Velusaamy <gokulvelusaamy@gmail.com>
2018-09-07 16:55:22 +0530

    Add files via upload

---
+++ Python/Day4/exception_handling.py
@@ -0,0 +1,13 @@
+try:
+	print 1/0
+except ZeroDivisionError:
+	print("0 division error")
+except Exception as e:
+	print(e)
+	
+try:
+	print 1/0
+except ZeroDivisionError:
+	print("0 division error")
+except Exception as e:
+	print(e)		
