4e7c96a8e5b245cc7628a720cd970ca306e9fe22
sandyashasha <45485372+sandyashasha@users.noreply.github.com>
2018-12-05 10:26:45 +0530

    Update Solutions.sql

--- SQL/ProblemSet00/Solutions.sql
+++ SQL/ProblemSet00/Solutions.sql
@@ -1,4 +1,26 @@
-ProblemSet00, August 06 2018
+ProblemSet00, december 05 , 2018
 --sandya.nandakumar@accenture.com
+
+Create table employee(e_id varchar(10),name varchar(20),dep_id varchar(10),salary int(10),manager_id varchar(10));
+insert into employee values("A114","MARTIN TREDEAU","D01",54497,"A120");
+insert into employee values("A116","ROBIN WAYNE","D02",20196,"A187");
+insert into employee values("A178","BRUCE WILLS","D03",66861,"A298");
+insert into employee values("A132","PAUL VINCENT","D01",94791,"A120");
+insert into employee values("A198","TOM HANKS","D02",16879,"A187");
+insert into employee values("A120","TIM ARCHER","D01",48834,"A298");
+insert into employee values("A143","BRAD MICHAEL","D01",24488,"A120");
+insert into employee values("A187","ADAM NJUSTIN","D02",80543,"A298");
+insert into employee values("A121","STUART WILLIAM","D02",78629,"A187");
+insert into employee values("A187","ROBERT SWIFT","D04",27700,"A298");
+insert into employee values("A176","EDWARD CANE","D01",898176,"A120");
+insert into employee values("A142","TARA CUMMINGS","D04",99475,"A187");
+insert into employee values("A130","VANESSA PARY","D04",28565,"A187");
+insert into employee values("A128","ADAM WAYNE","D05",94324,"A165");
+insert into employee values("A129","JOSEPH ANGELIN","D05",44280,"A165");
+insert into employee values("A165","NATASHA STEVENS","D05",31377,"A298");
+insert into employee values("A111","JOHN HELLEN","D01",15380,"A120");
+insert into employee values("A194","HAROLLD STEVENS","D02",32166,"A187");
+insert into employee values("A133","STEVE MICHELOS","D02",61215,"A187");
+insert into employee values("A156","NICK MARTIN","D03",50174,"A178");
 
 /* <Question from the ProblemSet> */
